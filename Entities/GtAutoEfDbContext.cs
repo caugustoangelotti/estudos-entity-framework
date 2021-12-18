4efa30e1b24d4b45f7573790bee2ae680f416a43
caugustoangelotti <carlos.angelotti@aluno.ic.ufmt.br>
2021-12-17 22:23:24 -0400

    definicao do relacionamento entre as tabelas

--- Entities/GtAutoEfDbContext.cs
+++ Entities/GtAutoEfDbContext.cs
@@ -4,12 +4,132 @@ namespace gtauto_api.Entities
 {
     public class GtAutoEfDbContext : DbContext
     {
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Funcionario> Funcionarios { get; set; }
+        public DbSet<Filial> Filiais { get; set; }
+        public DbSet<Veiculo> Veiculos { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Telefone> Telefones { get; set; }
+        public DbSet<Aluguel> Alugueis { get; set; }
+        public DbSet<Devolucao> Devolucoes { get; set; }
+
         public GtAutoEfDbContext(DbContextOptions<GtAutoEfDbContext> options)
             :base(options) { }
         
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            #region Endereco Relations
+                 
+            modelBuilder.Entity<Telefone>()
+                .HasOne(c => c.Cliente)
+                .WithMany(t => t.Telefones)
+                .HasForeignKey(c => c.IdCliente)
+                .OnDelete(DeleteBehavior.NoAction);
+
+             modelBuilder.Entity<Telefone>()
+                .HasOne(f => f.Funcionario)
+                .WithMany(t => t.Telefones)
+                .HasForeignKey(f => f.IdFuncionario)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Telefone>()
+                .HasOne(f => f.Filial)
+                .WithMany(t => t.Telefones)
+                .HasForeignKey(f => f.IdFilial)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            #endregion
+
+            #region Telefone Relations
+                 
+            modelBuilder.Entity<Endereco>()
+                .HasOne(c => c.Cliente)
+                .WithMany(e => e.Enderecos)
+                .HasForeignKey(c => c.IdCliente)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Endereco>()
+                .HasOne(f => f.Funcionario)
+                .WithMany(e => e.Enderecos)
+                .HasForeignKey(f => f.IdFuncionario)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Endereco>()
+                .HasOne(f => f.Filial)
+                .WithMany(e => e.Enderecos)
+                .HasForeignKey(f => f.IdFilial)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            #endregion
+
+            #region Veiculo Relations
+                 
+            modelBuilder.Entity<Veiculo>()
+                .HasOne(f => f.Filial)
+                .WithMany(v => v.Veiculos)
+                .HasForeignKey(f => f.IdFilial)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            #endregion
+
+            #region Alugueis Relations
+                
+            modelBuilder.Entity<Aluguel>()
+                .HasOne(c => c.Cliente)
+                .WithMany(a => a.Alugueis)
+                .HasForeignKey(c => c.IdCliente)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Aluguel>()
+                .HasOne(v => v.Veiculo)
+                .WithMany(a => a.Alugueis)
+                .HasForeignKey(v => v.IdVeiculo)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Aluguel>()
+                .HasOne(f => f.Funcionario)
+                .WithMany(a => a.Alugueis)
+                .HasForeignKey(f => f.IdFuncionario)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            modelBuilder.Entity<Aluguel>()
+                .HasOne(f => f.Filial)
+                .WithMany(a => a.Alugueis)
+                .HasForeignKey(f => f.IdFilial)
+                .OnDelete(DeleteBehavior.NoAction);
             
+            #endregion
+
+            #region Devolucao Relations
+            
+            modelBuilder.Entity<Devolucao>()
+                .HasOne(a => a.Aluguel)
+                .WithOne(d => d.Devolucao)
+                .HasForeignKey<Aluguel>(a => a.IdAluguel);
+                
+            modelBuilder.Entity<Devolucao>()
+                .HasOne(f => f.Funcionario)
+                .WithMany(d => d.Devolucoes)
+                .HasForeignKey(f => f.IdFuncionario)
+                .OnDelete(DeleteBehavior.NoAction);
+                    
+            modelBuilder.Entity<Devolucao>()
+                .HasOne(f => f.Filial)
+                .WithMany(d => d.Devolucoes)
+                .HasForeignKey(f => f.IdFilial)
+                .OnDelete(DeleteBehavior.NoAction);
+
+            #endregion
+
+            #region Funcionario Relations
+
+            modelBuilder.Entity<Funcionario>()
+                .HasOne(f => f.Filial)
+                .WithMany(f => f.Funcionarios)
+                .HasForeignKey(f => f.IdFilial)
+                .OnDelete(DeleteBehavior.NoAction);
+                 
+            #endregion
         }
     }
 }
